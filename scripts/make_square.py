8c19c63e64100d8a9c2a8a8dba9f3faf6476e236
xdze2 <xdze2.me@gmail.com>
2021-11-13 16:17:12 +0100

    Circle, look at stats

--- scripts/make_square.py
+++ scripts/make_square.py
@@ -2,19 +2,21 @@
 from pathlib import Path
 from jinja2 import Template
 
-from spotigraph.explore import get_second_gen
+from spotigraph.explore import get_second_gen, get_first_gen
 
 
 artist_id = "0wf6vuNqTvdRGrmpsPu2kW"
 
+first_gen = get_first_gen(artist_id)
 second_gen = get_second_gen(artist_id)
 
-print(second_gen)
+
 print(len(second_gen))
 
-top_one = second_gen.most_common(5)[0][0]
-print(top_one)
-
+for arti, count in second_gen.most_common(30):
+    is_in_first = arti in first_gen
+    is_in_first_label = "x" if is_in_first else ' '
+    print(f'{count:3d}', is_in_first_label, arti.name)
 
 template_src = """
 <html>
@@ -26,12 +28,13 @@ img {
     margin: 0;
     padding: 0;
     border: 0;
+    border-radius: 50%;
 }
 </style>
 <body>
 
 {% for arti in artists -%}
-    <img src="{{arti.image}}">
+    <img src="{{arti.image}}" width="{{arti.size}}px" height="{{arti.size}}px">
 {% endfor %}
 
 </body>
@@ -42,7 +45,13 @@ img {
 j2_template = Template(template_src)
 
 Path('output').mkdir(parents=True, exist_ok=True)
-data = [elt.asdict() for elt, count in second_gen.most_common()]
+data = [
+    {
+        **elt.asdict(),
+        'size':18 + 10*count
+    }
+    for elt, count in second_gen.most_common(30)
+]
 with open('output/square.html', 'w') as f:
     f.write(j2_template.render({'artists': data}))
 
